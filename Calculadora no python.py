d97d18e8436d5b333edf4d5ec1a32a1594547af5
Luc72639 <paulo.luc777@gmail.com>
2025-07-10 10:31:14 -0300

    Add files via upload

---
+++ Calculadora no python.py
@@ -0,0 +1,27 @@
+print(" Olá, vamos fazer alguns calculos")
+print("Vamos para adição")
+print('=== Soma ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+
+soma = a + b
+print('A soma é: '+str(soma))
+print("Agora vamos para subtração")
+print('=== Subtração ===')
+a = int(input('Digite mais um número: '))
+b = int(input('Digite outro número: '))
+Subtração = a - b
+print('A subtração é: '+str(Subtração))
+print("Vamos agora para multiplicação")
+print('=== Multiplicação ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+Multiplicação = a * b
+print('A Multiplicação é: '+str(Multiplicação))
+print("E para finalizar vamos para divisão")
+print('=== Divisão ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+Divisão = a / b
+print('O resultado é: '+str(Divisão))
+print("É isso, Tneha um bom dia!!!")
